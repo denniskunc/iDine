2cebabad60b302783f3a285ee3291eeabe36cc33
Dennis Kunc <dennis@ltstechnology.com>
2022-06-20 13:03:49 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Dennis Kunc on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Dennis Kunc on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
